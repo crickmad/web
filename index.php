9023275022a0d91f3cf5b837591fc0338422f015
crickmad <48318734+crickmad@users.noreply.github.com>
2022-10-07 18:00:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Developer News</h1>
+
+<?php echo "The Best PHP Examples"; ?>
+
+</body>
+</html> 
